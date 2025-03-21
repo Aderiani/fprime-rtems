fda3b59b7117c428d5a50c152f7ffed44e6ad4b0
Adrian <a.rezaeiadriani@gmail.com>
2025-03-21 14:04:54 +0100

    modifed condition

--- Os/RTEMS/Condition.cpp
+++ Os/RTEMS/Condition.cpp
@@ -1,97 +1,66 @@
 // ======================================================================
 // \title Os/RTEMS/ConditionVariable.cpp
-// \brief RTEMS implementation for Os::ConditionVariable
+// \brief RTEMS implementation for Os::ConditionVariable using POSIX API
 // ======================================================================
-#include "Os/Condition.hpp"
-#include "Os/RTEMS/Mutex.hpp"
+#include "Os/RTEMS/ConditionVariable.hpp"
 #include "Os/Delegate.hpp"
 #include <Fw/Types/Assert.hpp>
-#include <rtems.h>
-#include <errno.h>
-
+#include <pthread.h>
 namespace Os {
 namespace RTEMS {
 namespace ConditionVariable {
 
-struct RtemsConditionVariableHandle : public ConditionVariableHandle {
-    rtems_id condition_id;  // RTEMS condition variable identifier
-    RtemsConditionVariableHandle() : condition_id(0) {}
-};
+// Constructor
+RtemsConditionVariable::RtemsConditionVariable() {
+    int status = pthread_cond_init(&m_handle.condition_id, NULL);
+    FW_ASSERT(status == 0, static_cast<FwAssertArgType>(status));
+}
 
-class RtemsConditionVariable : public ConditionVariableInterface {
-  public:
-    //! Constructor
-    RtemsConditionVariable() {
-        // Create a RTEMS condition variable
-        rtems_status_code status = rtems_condition_variable_create(&m_handle.condition_id);
-        FW_ASSERT(status == RTEMS_SUCCESSFUL, static_cast<FwAssertArgType>(status));
-    }
+// Destructor
+RtemsConditionVariable::~RtemsConditionVariable() {
+    int status = pthread_cond_destroy(&m_handle.condition_id);
+    FW_ASSERT(status == 0, static_cast<FwAssertArgType>(status));
+}
 
-    //! Destructor
-    ~RtemsConditionVariable() override {
-        if (m_handle.condition_id != 0) {
-            rtems_status_code status = rtems_condition_variable_delete(m_handle.condition_id);
-            FW_ASSERT(status == RTEMS_SUCCESSFUL, static_cast<FwAssertArgType>(status));
-        }
-    }
+// Wait on condition_id variable
+ConditionVariableInterface::Status RtemsConditionVariable::pend(Os::Mutex& mutex) {
+    // Assuming Os::Mutex delegates to an Os::MutexInterface implementation
+    Os::MutexInterface* mutex_if = mutex.m_delegate;  // Adjusted based on F´ pattern
+    Os::RTEMS::Mutex::RTEMSMutex::MutexHandle* rtems_mutex_handle =
+        static_cast<Os::RTEMS::Mutex::RTEMSMutex::MutexHandle*>(mutex_if->getHandle());
 
-    //! Wait on condition variable
-    Status wait(Mutex& mutex) override {
-        // Get the RTEMS mutex from the Os::Mutex
-        Os::MutexInterface* mutex_if = mutex.getInterface();
-        RTEMSMutex::RTEMSMutexHandle* rtems_mutex_handle = 
-            static_cast<RTEMSMutex::RTEMSMutexHandle*>(mutex_if->getHandle());
-            
-        // Wait on the condition variable
-        rtems_status_code status = rtems_condition_variable_wait(
-            m_handle.condition_id, 
-            rtems_mutex_handle->mutex_id
-        );
-        
-        if (status == RTEMS_SUCCESSFUL) {
-            return Status::OK;
-        } else {
-            return Status::ERROR;
-        }
+    int status = pthread_cond_wait(&m_handle.condition_id, &rtems_mutex_handle->mutex);
+    if (status == 0) {
+        return Status::OP_OK;
+    } else {
+        return Status::ERROR_OTHER;
     }
+}
 
-    //! Signal one thread waiting on condition variable
-    Status signal() override {
-        rtems_status_code status = rtems_condition_variable_signal(m_handle.condition_id);
-        
-        if (status == RTEMS_SUCCESSFUL) {
-            return Status::OK;
-        } else {
-            return Status::ERROR;
-        }
-    }
+// Signal one thread
+void RtemsConditionVariable::notify() {
+    int status = pthread_cond_signal(&m_handle.condition_id);
+    FW_ASSERT(status == 0, static_cast<FwAssertArgType>(status));
+}
 
-    //! Broadcast to all threads waiting on condition variable
-    Status broadcast() override {
-        rtems_status_code status = rtems_condition_variable_broadcast(m_handle.condition_id);
-        
-        if (status == RTEMS_SUCCESSFUL) {
-            return Status::OK;
-        } else {
-            return Status::ERROR;
-        }
-    }
+// Broadcast to all threads
+void RtemsConditionVariable::notifyAll() {
+    int status = pthread_cond_broadcast(&m_handle.condition_id);
+    FW_ASSERT(status == 0, static_cast<FwAssertArgType>(status));
+}
 
-    //! Get handle to condition variable
-    ConditionVariableHandle* getHandle() override {
-        return &m_handle;
-    }
-    
-  private:
-    RtemsConditionVariableHandle m_handle;
-};
+// Get handle to condition_id variable
+ConditionVariableHandle* RtemsConditionVariable::getHandle() {
+    return &m_handle;
+}
 
 } // namespace ConditionVariable
 } // namespace RTEMS
 } // namespace Os
 
 namespace Os {
+// Delegate factory function
 ConditionVariableInterface* ConditionVariableInterface::getDelegate(ConditionVariableHandleStorage& aligned_new_memory) {
     return Os::Delegate::makeDelegate<ConditionVariableInterface, Os::RTEMS::ConditionVariable::RtemsConditionVariable>(aligned_new_memory);
 }
-}
+} // namespace Os
--- Os/RTEMS/Condition.hpp
+++ Os/RTEMS/Condition.hpp
@@ -1,24 +1,25 @@
 // ======================================================================
 // \title Os/RTEMS/ConditionVariable.hpp
-// \brief RTEMS implementation for Os::ConditionVariable
+// \brief RTEMS-specific declarations for Os::ConditionVariable
 // ======================================================================
-#ifndef OS_RTEMS_CONDITIONVARIABLE_HPP
-#define OS_RTEMS_CONDITIONVARIABLE_HPP
 
-#include <Os/Condition.hpp>
-#include <rtems.h>
+#ifndef OS_RTEMS_CONDITION_VARIABLE_HPP_
+#define OS_RTEMS_CONDITION_VARIABLE_HPP_
+
+#include "Os/Condition.hpp"
+#include "Os/RTEMS/Mutex.hpp"
+#include <pthread.h>  // For POSIX condition variables
 
 namespace Os {
 namespace RTEMS {
 namespace ConditionVariable {
 
-//! ConditionVariableHandle class definition for RTEMS implementations
+//! \brief RTEMS-specific condition variable handle
 struct RtemsConditionVariableHandle : public ConditionVariableHandle {
-    rtems_id condition_id;  // RTEMS condition variable identifier
-    RtemsConditionVariableHandle() : condition_id(0) {}
+    pthread_cond_t condition;  //!< POSIX condition variable
 };
 
-//! \brief RTEMS implementation of Os::ConditionVariableInterface
+//! \brief RTEMS implementation of ConditionVariableInterface
 class RtemsConditionVariable : public ConditionVariableInterface {
   public:
     //! Constructor
@@ -28,23 +29,23 @@ class RtemsConditionVariable : public ConditionVariableInterface {
     ~RtemsConditionVariable() override;
 
     //! Wait on condition variable
-    Status wait(Mutex& mutex) override;
+    Status pend(Os::Mutex& mutex) override;
 
-    //! Signal one thread waiting on condition variable
-    Status signal() override;
+    //! Signal one thread
+    void notify() override;
 
-    //! Broadcast to all threads waiting on condition variable
-    Status broadcast() override;
+    //! Broadcast to all threads
+    void notifyAll() override;
 
     //! Get handle to condition variable
     ConditionVariableHandle* getHandle() override;
-    
+
   private:
-    RtemsConditionVariableHandle m_handle;
+    RtemsConditionVariableHandle m_handle;  //!< Internal handle storage
 };
 
 } // namespace ConditionVariable
 } // namespace RTEMS
 } // namespace Os
 
-#endif // OS_RTEMS_CONDITIONVARIABLE_HPP
+#endif // OS_RTEMS_CONDITION_VARIABLE_HPP_
