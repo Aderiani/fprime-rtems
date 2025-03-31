f533baffea6d7808ecc562147e9faeef878dd6e9
Adrian <a.rezaeiadriani@gmail.com>
2025-03-31 15:27:39 +0200

    moved smp to single core

--- LedBlinker/Main.cpp
+++ LedBlinker/Main.cpp
@@ -11,6 +11,11 @@
 
 #ifdef __rtems__
 #include <rtems.h>
+// Add this for direct console output that doesn't rely on OS services
+#include <stdio.h>
+#define DEBUG_PRINT(msg) printf("%s\n", msg)
+#else
+#define DEBUG_PRINT(msg) /* empty in non-RTEMS builds */
 #endif
 
 // External network initialization functions (optional, only for RTEMS)
@@ -33,61 +38,171 @@ extern void cleanup_network_config(NetworkConfig* config);
 #endif
 
 // Logging wrapper to handle different Logger interfaces
-namespace {
-void safeLogAdd(const char* message) {
-    Fw::Logger::log(message);
-}
-}  // namespace
+// namespace {
+// void safeLogAdd(const char* message) {
+//     // For RTEMS, use direct printf for initialization debugging
+// #ifdef __rtems__
+//     printf("[F'] %s\n", message);
+// #else
+//     Fw::Logger::log(message);
+// #endif
+// }
+// }  // namespace
 
 // External function for setting up the topology
+// extern "C" int fprime_main(int argc, char* argv[]) {
+//     DEBUG_PRINT("fprime_main started");
+    
+//     // Initialize OSAL - THIS IS WHERE IT'S HANGING
+//     DEBUG_PRINT("Calling Os::init()");
+//     Os::init();
+//     DEBUG_PRINT("Os::init() completed");
 
-extern "C" int fprime_main(int argc, char* argv[]) {
-    printf("F' main starting...\n");
-    
-    // Initialize OSAL
-    printf("Initializing OS...\n");
-    Os::init();
-    printf("OS initialized successfully\n");
-    
-    // Setup topology
-    printf("Setting up topology...\n");
-    LedBlinker::TopologyState inputs;
-    inputs.hostname = "0.0.0.0";
-    inputs.port = 50000;
-    
-    try {
-        LedBlinker::setupTopology(inputs);
-        printf("Topology setup complete\n");
-        
-        printf("Starting simulated cycle...\n");
-        LedBlinker::startSimulatedCycle(Fw::TimeInterval(1, 0));
-        printf("Simulated cycle started\n");
-        
-        printf("Running for 10 seconds...\n");
-        rtems_task_wake_after(rtems_clock_get_ticks_per_second() * 10);
-        
-        printf("Stopping simulated cycle...\n");
-        LedBlinker::stopSimulatedCycle();
-        printf("Simulated cycle stopped\n");
-        
-        printf("Tearing down topology...\n");
-        LedBlinker::teardownTopology(inputs);
-        printf("Topology torn down\n");
-        
-        printf("F' application completed successfully\n");
-        return 0;
-    } catch (const std::exception& e) {
-        printf("Exception during F' execution: %s\n", e.what());
-        return -1;
-    } catch (...) {
-        printf("Unknown exception during F' execution\n");
-        return -1;
-    }
-}
+//     // Default configuration
+//     int use_dhcp = 0;                        // Set to 0 to use static IP
+//     const char* static_ip = "192.168.1.10";  // Network's static IP
+//     const char* netmask = "255.255.255.0";
+//     const char* gateway = "192.168.1.1";  
+//     U16 port = 50000;                     // Default port
+
+//     // Parse command-line arguments
+//     for (int i = 1; i < argc; ++i) {
+//         if (strcmp(argv[i], "--no-dhcp") == 0) {
+//             use_dhcp = 0;
+//         } else if (strcmp(argv[i], "--ip") == 0 && i + 1 < argc) {
+//             static_ip = argv[++i];
+//         } else if (strcmp(argv[i], "--netmask") == 0 && i + 1 < argc) {
+//             netmask = argv[++i];
+//         } else if (strcmp(argv[i], "--gateway") == 0 && i + 1 < argc) {
+//             gateway = argv[++i];
+//         } else if (strcmp(argv[i], "--port") == 0 && i + 1 < argc) {
+//             port = static_cast<U16>(std::atoi(argv[++i]));
+//         }
+//     }
+
+// // Network initialization (RTEMS-specific)
+// #ifdef __rtems__
+//     DEBUG_PRINT("Creating network config");
+//     NetworkConfig* net_config = create_network_config(use_dhcp, static_ip, netmask, gateway);
+
+//     if (!net_config) {
+//         safeLogAdd("Failed to create network configuration");
+//         return -1;
+//     }
+
+//     DEBUG_PRINT("Initializing network");
+//     if (initialize_network(net_config) != 0) {
+//         safeLogAdd("Network initialization failed");
+//         cleanup_network_config(net_config);
+//         return -1;
+//     }
+
+//     cleanup_network_config(net_config);
+//     DEBUG_PRINT("Network initialized");
+// #endif
+
+//     // Object for communicating state to the topology
+//     LedBlinker::TopologyState inputs;
+//     inputs.hostname = static_ip ? static_ip : "0.0.0.0";
+//     inputs.port = port;
+
+//     // Logging initialization
+//     safeLogAdd("Starting F' Application");
+
+//     // Setup topology
+//     DEBUG_PRINT("Setting up topology");
+//     LedBlinker::setupTopology(inputs);
+//     DEBUG_PRINT("Topology setup complete");
+
+//     // Start simulated cycle
+//     DEBUG_PRINT("Starting simulated cycle");
+//     LedBlinker::startSimulatedCycle(Fw::TimeInterval(1, 0));
+//     DEBUG_PRINT("Simulated cycle started");
+
+// // Platform-specific run mechanism
+// #ifdef __rtems__
+//     // RTEMS-specific delay
+//     DEBUG_PRINT("Delaying for 60 seconds");
+//     rtems_task_wake_after(rtems_clock_get_ticks_per_second() * 60);
+// #else
+//     // Generic time-based delay for non-RTEMS platforms
+//     Os::Task::delay(Fw::TimeInterval(60, 0));
+// #endif
+
+//     // Stop simulated cycle
+//     DEBUG_PRINT("Stopping simulated cycle");
+//     LedBlinker::stopSimulatedCycle();
+//     DEBUG_PRINT("Simulated cycle stopped");
+
+//     // Teardown topology
+//     DEBUG_PRINT("Tearing down topology");
+//     LedBlinker::teardownTopology(inputs);
+//     DEBUG_PRINT("Topology teardown complete");
+//     printf("Exiting fprime_main successfully\n");
+//     return 0;
+// }
+
 // RTEMS requires a special main for C++ applications
+// #ifdef __rtems__
+// extern "C" int main(int argc, char* argv[]) {
+//     printf("RTEMS main() starting...\n");
+//     int result = fprime_main(argc, argv);
+//     printf("RTEMS main() completed with result: %d\n", result);
+//     return result;
+// }
+// #else
+// // Standard main for non-RTEMS platforms
+// int main(int argc, char* argv[]) {
+//     return fprime_main(argc, argv);
+// }
+// #endif
+// RTEMS requires a special main for C++ applications
+// RTEMS requires a special main for C++ applications
+
+
+// Modify the fprime_main function to also not return normally
+extern "C" int fprime_main(int argc, char* argv[]) {
+    printf("fprime_main started\n");
+    
+    // Initialize OSAL - modified to do nothing for RTEMS
+    printf("Calling Os::init()\n");
+    Os::init();
+    printf("Os::init() completed\n");
+    
+    // Add more F Prime operations here as needed
+    
+    printf("fprime_main operations completed\n");
+    
+#ifdef __rtems__
+    // For RTEMS, don't return normally
+    printf("fprime_main returning control to main\n");
+    return 0;
+#else
+    // For other platforms, return normally
+    return 0;
+#endif
+}
+
+
+
 #ifdef __rtems__
 extern "C" int main(int argc, char* argv[]) {
-    return fprime_main(argc, argv);
+    printf("RTEMS main() starting...\n");
+    
+    // Call fprime_main but don't use its return value
+    fprime_main(argc, argv);
+    
+    printf("RTEMS main() entering permanent idle state\n");
+    
+    // Force the application to remain running indefinitely
+    // This avoids destructors and C++ runtime cleanup that may be causing issues
+    for(;;) {
+        // Sleep for 1 second
+        rtems_task_wake_after(rtems_clock_get_ticks_per_second());
+    }
+    
+    // This line will never be reached
+    return 0;
 }
 #else
 // Standard main for non-RTEMS platforms
@@ -95,3 +210,4 @@ int main(int argc, char* argv[]) {
     return fprime_main(argc, argv);
 }
 #endif
+
--- Os/Os.cpp
+++ Os/Os.cpp
@@ -10,11 +10,19 @@
 #include "Os/Memory.hpp"
 #include "Os/Task.hpp"
 
+// For debugging during initialization
+#ifdef __rtems__
+#include <stdio.h>
+#define OS_INIT_DEBUG(msg) printf("OS Init: %s\n", msg)
+#else
+#define OS_INIT_DEBUG(msg) /* empty in non-RTEMS builds */
+#endif
 namespace Os {
 
 void init() {
-    // Initialize all OSAL singletons
+
     Os::Console::init();
+    OS_INIT_DEBUG("Console initialized");
     Os::FileSystem::init();
     Os::Cpu::init();
     Os::Memory::init();
