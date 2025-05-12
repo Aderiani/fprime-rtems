d47641abd1db939b7833360e2792c72b03f282fd
Adrian <a.rezaeiadriani@gmail.com>
2025-05-12 09:02:00 +0200

    removed the force send in telmemtery

--- LedBlinker/Top/LedBlinkerTopology.cpp
+++ LedBlinker/Top/LedBlinkerTopology.cpp
@@ -169,45 +169,7 @@ bool checkAndProcessTimerTick() {
     return true;
 }
 
-// void forceTelemetryDownlink() {
-//     printf("[FORCE-TLM] Manually forcing telemetry downlink\n");
-    
-//     // Create a telemetry packet directly
-//     Fw::ComBuffer buffer;
-//     buffer.resetSer();
-    
-//     // Add a packet descriptor (0 = telemetry)
-//     U32 descriptor = 0;
-//     Fw::SerializeStatus stat = buffer.serialize(descriptor);
-    
-//     // Add current time
-//     Fw::Time time;
-//     time.set(1, 0);  // Set to a fixed value for testing
-//     stat = buffer.serialize(time);
-    
-//     // Add channel ID (0x100)
-//     U32 id = 0x100;
-//     stat = buffer.serialize(id);
-    
-//     // Add value (heartbeat counter)
-//     static U32 counter = 0;
-//     counter++;
-//     stat = buffer.serialize(counter);
-    
-//     // Get the final buffer
-//     printf("[FORCE-TLM] Created telemetry buffer with size %llu\n", 
-//            buffer.getBuffLength());
-    
-//     // // Send directly to comQueue
-//     // if (comQueue.isConnected_comQueueSend_OutputPort(0)) {
-//     //     printf("[FORCE-TLM] Sending telemetry directly to comQueue\n");
-//     //     comQueue.comQueueIn_handler(0, buffer, 0);
-//     // } else {
-//     //     printf("[FORCE-TLM] ERROR: comQueue input port not connected!\n");
-//     // }
-    
-//     // printf("[FORCE-TLM] Force telemetry complete\n");
-// }
+
 
 void setupTopology(const TopologyState& state) {
     // Initialize components one by one
